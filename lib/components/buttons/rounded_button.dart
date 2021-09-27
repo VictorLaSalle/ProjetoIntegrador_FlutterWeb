9cbff9e98fdda031631f4258fb27f3e5a252b633
VictorLaSalle <victor.202031214@unilasalle.edu.br>
2021-09-27 09:40:34 -0300

    Componentes responsivos

--- lib/components/buttons/rounded_button.dart
+++ lib/components/buttons/rounded_button.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
 
 class RoundedButton extends StatelessWidget {
+  final String _text;
+  final Function() _onPressed;
+  final double _height;
+  final double _width = 140;
+  EdgeInsets? _margin = EdgeInsets.all(20);
 
-  final String text;
-  final Function() onPressed;
-  final double height;
-  final double width = 140;
-  
-  RoundedButton({
-    required this.text,
-    required this.onPressed,
-    required this.height
-  });
-  
+  RoundedButton({required text, required onPressed, required height, margin})
+      : this._text = text,
+        this._onPressed = onPressed,
+        this._height = height;
+
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: Text(this.text, style: TextStyle(color: Colors.orange)),
-      onPressed: this.onPressed,
-      style: ElevatedButton.styleFrom(
-        minimumSize: Size(width, height),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(50))
+    return Container(
+      margin: this._margin,
+      child: ElevatedButton(
+        child: Text(this._text, style: TextStyle(color: Colors.orange)),
+        onPressed: this._onPressed,
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(_width, _height),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(50))),
+          primary: Colors.white,
         ),
-        primary: Colors.white,
       ),
     );
   }
--- lib/components/textfield/white_textfield.dart
+++ lib/components/textfield/white_textfield.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
 class WhiteTextField extends StatelessWidget {
-
   final String _hintText;
   final bool? _obscureText;
   final TextStyle _textStyle = TextStyle(color: Colors.white);
+  final double _width;
+  EdgeInsets _margin;
 
-  WhiteTextField(this._hintText, {obscureText = false}) : this._obscureText = obscureText;
+  WhiteTextField(this._hintText, width, margin, {obscureText = false})
+      : this._width = width,
+        this._obscureText = obscureText,
+        this._margin = margin;
 
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width * 0.15,
+      margin: this._margin,
+      width: _width,
       child: TextFormField(
         style: this._textStyle,
         cursorColor: Colors.white,
@@ -20,15 +25,10 @@ class WhiteTextField extends StatelessWidget {
             hintStyle: this._textStyle,
             enabledBorder: UnderlineInputBorder(
                 borderSide: BorderSide(
-                  color: Colors.white,
-                )
-            ),
+              color: Colors.white,
+            )),
             focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(
-                    color: Color(0xff053D7A)
-                )
-            )
-        ),
+                borderSide: BorderSide(color: Color(0xff053D7A)))),
       ),
     );
   }
